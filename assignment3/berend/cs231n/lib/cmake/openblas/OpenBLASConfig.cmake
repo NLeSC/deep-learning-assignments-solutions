c2790801ca7594b1b6a9dc110f375b9fa3dba10c
Berend Weel <b.weel@esciencecenter.nl>
2016-09-06 11:59:43 +0200

    Assignment 3 by Berend

---
+++ assignment3/berend/cs231n/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,3 @@
+SET(OpenBLAS_VERSION "0.2.14")
+SET(OpenBLAS_INCLUDE_DIRS /home/bweel/Documents/projects/cs231n/deep-learning-assignments-solutions/assignment3/berend/cs231n/include)
+SET(OpenBLAS_LIBRARIES /home/bweel/Documents/projects/cs231n/deep-learning-assignments-solutions/assignment3/berend/cs231n/lib/libopenblas.so)
